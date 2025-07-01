22ce7763169ea83662b3c14da32a637eacce711a
gptjd1997 <gptjd1997@gmail.com>
2025-07-01 21:51:29 +0900

    ✨ Feat: NextAuth JWT 세션 전략 및 소셜 로그인 연동
    - NextAuth 세션 전략을 데이터베이스에서 JWT로 변경
    - Google 및 Kakao 소셜 로그인 프로바이더 연동
    - JWT 및 세션 콜백 로깅 추가

--- src/auth.ts
+++ src/auth.ts
@@ -2,17 +2,34 @@ import NextAuth from "next-auth";
 import authConfig from "./auth.config";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { prisma } from "@/lib/prisma";
+import Kakao from "next-auth/providers/kakao";
+import Google from "next-auth/providers/google";
+
+// console.log("process.env.AUTH_SECRET", process.env.AUTH_SECRET);
+// console.log("process.env.AUTH_GOOGLE_ID", process.env.AUTH_GOOGLE_ID);
+// console.log("process.env.AUTH_GOOGLE_SECRET", process.env.AUTH_GOOGLE_SECRET);
+// console.log("process.env.AUTH_KAKAO_ID", process.env.AUTH_KAKAO_ID);
+// console.log("process.env.AUTH_KAKAO_SECRET", process.env.AUTH_KAKAO_SECRET);
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: PrismaAdapter(prisma),
-  secret: process.env.AUTH_SECRET,
+  session: {
+    strategy: "jwt",
+  },
   debug: true,
-
-  ...authConfig,
-
+  secret: process.env.AUTH_SECRET,
+  providers: [Google, Kakao],
   callbacks: {
+    async jwt({ token, user }) {
+      console.log("token", token);
+      console.log("user", user);
+
+      return token;
+    },
     async session({ session, user }) {
-      console.log(user);
+      console.log("session", session);
+      console.log("user", user);
+
       return session;
     },
   },
